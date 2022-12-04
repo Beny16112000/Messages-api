c1feb974f72e7c9aab8563b767c6456905f59ee1
benjamin shtainberg <104722639+Beny16112000@users.noreply.github.com>
2022-12-04 13:58:53 +0200

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('messages',views.MessagesAll.as_view()),
     path('messages/login',obtain_auth_token),
-    path('messages/unread',views.unread_messages,name='unread_messages'),
+    path('messages/unread',views.MessagesUnread.as_view()),
     path('messages/<int:id>',views.Messages.as_view()),
 ]
