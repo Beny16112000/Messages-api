10fcd3babee3b8d9d23ed9ffd379ad1135e98b4a
benjamin shtainberg <104722639+Beny16112000@users.noreply.github.com>
2022-12-07 03:18:16 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,12 +2,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Message
 from .serializers import MessageSerializers
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 
 
 """
@@ -25,13 +25,14 @@ class MessagesAll(APIView):
 
     def post(self, request):
         data = JSONParser().parse(request)
-        receiver = User.objects.filter(id=data['receiver'])
-        if not receiver:
-            return Response('You cannot send him a message because the user does not exist, Please enter existing user id', status=status.HTTP_404_NOT_FOUND)
-        else:
-            message = Message(sender=request.user,receiver=receiver[0],subject=data['subject'],message=data['message'])
+        try:
+            receiver = User.objects.get(id=data['receiver'])
+            message = Message(sender=request.user,receiver=receiver
+                ,subject=data['subject'],message=data['message'])
             message.save()
             return Response('The message has been sent', status=status.HTTP_201_CREATED)
+        except ObjectDoesNotExist:
+            return Response('You cannot send him a message because the user does not exist, Please enter existing user id', status=status.HTTP_404_NOT_FOUND)
 
 
     def get(self, request):
@@ -42,7 +43,7 @@ class MessagesAll(APIView):
             serializer = MessageSerializers(data, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        
+
 
 class MessagesUnread(APIView):
     permission_classes = [IsAuthenticated]
@@ -50,7 +51,7 @@ class MessagesUnread(APIView):
     def get(self, request):
         data = Message.objects.filter(receiver=request.user,read=False)
         if not data:
-            return Response('It appears that you do not have any unread messages', status=status.HTTP_404_NOT_FOUND)
+            return Response('There is not unread messages for you', status=status.HTTP_404_NOT_FOUND)
         else:
             serializer = MessageSerializers(data, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -61,23 +62,26 @@ class Messages(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self,request,id):
-        data = Message.objects.filter(id=id,receiver=request.user)
-        if not data:
-            return Response('You must be as receiver and have a message with the message ID', status=status.HTTP_404_NOT_FOUND)
-        else:
-            data[0].read = True
-            data[0].save()
-            serializer = MessageSerializers(data[0])
+        try:
+            data = Message.objects.get(id=id,receiver=request.user)
+            data.read = True
+            data.save()
+            serializer = MessageSerializers(data)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response('You must be as receiver and have a message with the message ID', status=status.HTTP_404_NOT_FOUND)
 
 
     def delete(self,request,id):
-        data = Message.objects.filter(id=id)
-        if not data:
-            return Response('This ID does not have any message', status=status.HTTP_404_NOT_FOUND)
-        else:
-            if data[0].sender == request.user or data[0].receiver == request.user:
-                data[0].delete()
+        try:
+            data = Message.objects.get(id=id)
+            if data.sender == request.user or data.receiver == request.user:
+                data.delete()
+                data.save()
                 return Response('Message deleted', status=status.HTTP_200_OK)
             else:
                 return Response('You need to be the sender or the receiver to delete the message', status=status.HTTP_404_NOT_FOUND)
+        except ObjectDoesNotExist:
+            return Response('This ID does not have any message', status=status.HTTP_404_NOT_FOUND)
+        
+        
