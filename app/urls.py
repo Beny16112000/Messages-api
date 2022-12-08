7e0f600c0d2fc144daf234606907ef43ced32a79
benjamin shtainberg <104722639+Beny16112000@users.noreply.github.com>
2022-12-08 21:53:00 +0200

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
+router.register('',views.MessagesAll, basename='messages')
 
 urlpatterns = [
-    path('messages',views.MessagesAll.as_view()),
     path('messages/login',obtain_auth_token),
-    path('messages/unread',views.MessagesUnread.as_view()),
-    path('messages/<int:id>',views.Messages.as_view()),
+    path('messages/',include(router.urls)),
+    path('messages/unread', views.MessagesAll.as_view({'get': 'unreadMessages'}))
 ]
